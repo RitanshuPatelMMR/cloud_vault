3c072f3c7adceb62d03e45f5d7d55f3090d183b1
ritanshupatel <ritanshupatel2001@gmail.com>
2026-01-03 23:40:39 +0530

    feat(ui): add responsive mobile navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,9 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import SideBar from "@/components/SideBar";
 import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
-import { avatarPlaceholderUrl } from "@/constants";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
@@ -14,7 +13,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <main className="flex h-screen">
       <SideBar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation />
+        <MobileNavigation {...currentUser} />
         <Header />
         <div className="main-contant">{children}</div>
       </section>
