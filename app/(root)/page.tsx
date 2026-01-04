b7ae776c894013671d4f7ff440d2c37d7accc93f
ritanshu patel <ritanshupatel.mmr@gmail.com>
2026-01-04 22:44:39 +0530

    feat: implement dynamic file routing and file cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex-center h-screen bg-gray-400">
+    <div className="flex-center h-screen bg-gray-100">
       <h1 className="h1">Cloud Vault - The only storage solution you need</h1>
     </div>
   );
